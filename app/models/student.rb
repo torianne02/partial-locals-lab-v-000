37cdfd44cedba05fed23a89acad44c2ee57f7554
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-22 22:10:31 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,8 +17,8 @@ class Student < ActiveRecord::Base
   def self.search(user_input)
     if !user_input.empty?
       where('NAME like ?', "%#{user_input}%")
-    else 
+    else
       self.all
-    end 
+    end
   end
 end
