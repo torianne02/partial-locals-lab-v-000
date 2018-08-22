2560ee06f3cdedd89e87c03deac620872c1a8cbe
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-22 22:11:07 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:user_input])
   end
 
   def student_params
